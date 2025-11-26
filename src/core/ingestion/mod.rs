31fd31946dc19bd8d45877ca176ec63e4bfdad24
jakob <lilliemarckjakob@gmail.com>
2025-11-26 08:59:27 +0100

    feat: tests for feedforwaerd model

---
+++ src/core/ingestion/mod.rs
@@ -0,0 +1,9 @@
+mod ingestion;
+
+pub use ingestion::Cast;
+pub use ingestion::Csv;
+pub use ingestion::Error;
+pub use ingestion::Extract;
+pub use ingestion::Ingestable;
+pub use ingestion::ManySequences;
+pub use ingestion::Sequence;
