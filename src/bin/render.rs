d9ace0e5e8f0e6c117d8110b163eedb6a9d60104
jakob <lilliemarckjakob@gmail.com>
2025-12-07 06:43:58 +0100

    feat: writing

--- src/bin/render.rs
+++ src/bin/render.rs
@@ -1,25 +1,68 @@
 use std::path::PathBuf;
 
 use askama::Template;
+use csv::ReaderBuilder;
 use fx_durable_ga_app::book::chapters::Chapter1;
-use fx_durable_ga_app::book::charts::{Chart, ChartDataset, LineChartData};
+use fx_durable_ga_app::book::charts::{Chart, ChartDataset, ChartOptions, LineChartData};
 use uuid::Uuid;
 
 const AUTHOR: &str = "Jakob Lilliemarck";
 
+fn load_csv(csv_path: &PathBuf) -> Result<LineChartData, Box<dyn std::error::Error>> {
+    let mut reader = ReaderBuilder::new().has_headers(true).from_path(csv_path)?;
+
+    let mut labels = Vec::new();
+    let mut data = Vec::new();
+
+    for result in reader.records() {
+        let record = result?;
+
+        let fitness = record
+            .get(0)
+            .and_then(|s| s.parse::<f32>().ok())
+            .ok_or("Invalid fitness value")?;
+
+        let generation = record
+            .get(1)
+            .and_then(|s| s.parse::<i32>().ok())
+            .ok_or("Invalid generation ID")?;
+
+        labels.push(format!("Gen {}", generation));
+        data.push(fitness);
+    }
+
+    let fitness_dataset = ChartDataset {
+        label: "Average Fitness".into(),
+        data,
+        border_color: Some("rgb(75, 192, 192)".into()),
+        tension: Some(0.1),
+        point_radius: Some(0.0),
+        border_width: Some(1.5),
+    };
+
+    let trend_dataset = fitness_dataset.trend();
+
+    Ok(LineChartData {
+        labels,
+        datasets: vec![fitness_dataset, trend_dataset],
+    })
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Create chart from data (later: load from CSV)
+    let manifest_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    let csv_path = manifest_dir
+        .join("data")
+        .join("average_fitness_by_generation_2025-12-06T15_09_17.586384524Z.csv");
+
+    // Load chart data from CSV
+    let chart_data = load_csv(&csv_path)?;
     let chart = Chart::Line {
         id: Uuid::now_v7(),
-        data: LineChartData {
-            labels: vec!["Jan".into(), "Feb".into(), "Mar".into()],
-            datasets: vec![ChartDataset {
-                label: "Sales".into(),
-                data: vec![65.0, 59.0, 80.0],
-                border_color: Some("rgb(255, 0, 128)".into()),
-                tension: Some(0.1),
-            }],
-        },
+        data: chart_data,
+        options: Some(ChartOptions {
+            animation: false,
+            aspect_ratio: Some(1.7),
+        }),
     };
 
     // Render chart to HTML
@@ -35,7 +78,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     };
 
     // Render and save
-    let manifest_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
     let book_dir = manifest_dir.join("book");
     let rendered_html = chapter.render()?;
     let output_path = book_dir.join("chapter-1.html");
--- src/book/charts.rs
+++ src/book/charts.rs
@@ -4,13 +4,13 @@ use uuid::Uuid;
 
 #[derive(Template, Serialize)]
 #[template(path = "charts/line.html")]
-#[serde(tag = "type", content = "data", rename_all = "camelCase")]
+#[serde(tag = "type", rename_all = "camelCase")]
 pub enum Chart {
     Line {
         #[serde(skip)]
         id: Uuid,
-        #[serde(flatten)]
         data: LineChartData,
+        options: Option<ChartOptions>,
     },
 }
 
@@ -35,6 +35,14 @@ pub struct LineChartData {
 
 #[derive(Serialize)]
 #[serde(rename_all = "camelCase")]
+pub struct ChartOptions {
+    pub animation: bool,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub aspect_ratio: Option<f32>,
+}
+
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
 pub struct ChartDataset {
     pub label: String,
     pub data: Vec<f32>,
@@ -42,4 +50,43 @@ pub struct ChartDataset {
     pub border_color: Option<String>,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub tension: Option<f32>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub point_radius: Option<f32>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub border_width: Option<f32>,
+}
+
+impl ChartDataset {
+    pub fn trend(&self) -> ChartDataset {
+        let n = self.data.len() as f32;
+
+        let mut sum_y = 0.0;
+        let mut sum_xy = 0.0;
+
+        for (i, &y) in self.data.iter().enumerate() {
+            let x = i as f32;
+            sum_y += y;
+            sum_xy += x * y;
+        }
+
+        let x_mean = (n - 1.0) / 2.0;
+        let y_mean = sum_y / n;
+        let sum_x2 = n * (n - 1.0) * (2.0 * n - 1.0) / 6.0;
+
+        let slope = (sum_xy - n * x_mean * y_mean) / (sum_x2 - n * x_mean * x_mean);
+        let intercept = y_mean - slope * x_mean;
+
+        let trend_data = (0..self.data.len())
+            .map(|i| slope * i as f32 + intercept)
+            .collect();
+
+        ChartDataset {
+            label: format!("Trend: {}", self.label),
+            data: trend_data,
+            border_color: Some("rgb(255, 99, 132)".into()),
+            tension: Some(0.0),
+            point_radius: Some(0.0),
+            border_width: Some(1.0),
+        }
+    }
 }
